4b7a5bdb5a27f8546bb3f5d8a0b58b739659ff0a
MNour7 <mahamadou.nour7@gmail.com>
2016-11-20 11:27:41 +0100

    Modification class Element et Obstacle

--- src/wargame/Element.java
+++ src/wargame/Element.java
@@ -1,5 +1,18 @@
 package wargame;
 
-public class Element {
+import java.awt.Color;
+import java.awt.Graphics;
 
+public class Element {
+	protected Color coul;
+	protected Position pos;
+	
+	//seDessiner chaque element sur la carte
+	public void seDessiner(Graphics g){
+		int[] xPoints ={pos.getX()*20,(pos.getX()+1)*20,(pos.getX()+1)*20,pos.getX()*20 };
+		int[] yPoints ={pos.getY()*20,pos.getY()*20,(pos.getY()+1)*20,(pos.getY()+1)*20};
+		g.setColor(coul);
+		g.fillPolygon(xPoints, yPoints, 4);
+	}
+	
 }
