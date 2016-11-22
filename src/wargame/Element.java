c55c8d9a27874a91d17643c9084ece2f8b1ff3ca
Ahmad AYADA <mr.ayadi@gmail.com>
2016-11-22 13:02:16 +0100

    rien

--- src/wargame/Element.java
+++ src/wargame/Element.java
@@ -9,8 +9,8 @@ public abstract class Element {
 	
 	//seDessiner chaque element sur la carte
 	public void seDessiner(Graphics g){
-		int[] xPoints ={pos.getX()*20,(pos.getX()+1)*20,(pos.getX()+1)*20,pos.getX()*20 };
-		int[] yPoints ={pos.getY()*20,pos.getY()*20,(pos.getY()+1)*20,(pos.getY()+1)*20};
+		int[] xPoints ={pos.getX()*20,(pos.getX()+1)*20,(pos.getX()+2)*20,(pos.getX()+3)*20 };
+		int[] yPoints ={pos.getY()*20,(pos.getY()+1)*20,(pos.getY()+2)*20,(pos.getY()+3)*20 };
 		g.setColor(coul);
 		g.fillPolygon(xPoints, yPoints, 4);
 	}
