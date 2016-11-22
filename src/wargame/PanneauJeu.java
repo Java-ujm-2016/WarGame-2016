11aee31e03e41e462c3c3cf988c8213797fe9f6b
Ahmad AYADA <mr.ayadi@gmail.com>
2016-11-22 13:00:19 +0100

    Modification PanneauJeau

--- src/wargame/PanneauJeu.java
+++ src/wargame/PanneauJeu.java
@@ -2,6 +2,7 @@ package wargame;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
@@ -10,12 +11,19 @@ public class PanneauJeu extends JPanel{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	/* Variable **/
 
 	public PanneauJeu(){
-		super();
+		 
 		setOpaque(true);
 		setBackground(Color.GRAY);
 		setPreferredSize(new Dimension(IConfig.WIDTH, IConfig.HIGHT));
 	}
 
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		
+	}
 }
