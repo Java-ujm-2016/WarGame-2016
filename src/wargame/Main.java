2cbe7c99a6194a38cc7665c8f3824d4ead3c295b
Ahmad AYADA <mr.ayadi@gmail.com>
2016-11-22 12:16:03 +0100

    Adding Code multithreading

--- src/wargame/Main.java
+++ src/wargame/Main.java
@@ -1,11 +1,17 @@
 package wargame;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new MonFrame();
-		
+	
+		SwingUtilities.invokeLater( new Runnable() {
+			public void	run	() {
+
+				new MonFrame();
+		}});
 	}
 
 }
