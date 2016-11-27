4822d477f6216c6fd3b3a602635cccc61f9b800c
Ahmad AYADA <mr.ayadi@gmail.com>
2016-11-27 17:45:10 +0100

    modification add numeroSoldat

--- src/wargame/Element.java
+++ src/wargame/Element.java
@@ -14,7 +14,6 @@ public  class Element implements IConfig {
 	public void dessinerCarree(int x, int y, Graphics g) {
 		g.setColor(Color.yellow);
         if(numeroSoldat != 0)
-
 		    g.drawString(numeroSoldat+"",x + IConfig.NB_PIX_CASE, (y+IConfig.NB_PIX_CASE));
 	    g.setColor(IConfig.COULEUR_TEXTE);
 		//g.drawString("");
